ca8adba3d037518efd5198c9c6a428040a1bf0cb
Priyadharshini C <125353483+JPriyasathya@users.noreply.github.com>
2025-09-29 09:09:42 +0530

    Time: 7 ms (92.43%), Space: 45.8 MB (14.57%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n=names.length;
+        Map<Integer,String> map=new HashMap<>();
+        for(int i=0;i<n;++i)
+        {
+            map.put(heights[i],names[i]);
+        }
+        Arrays.sort(heights);
+        for(int i=0;i<n/2;i++)
+        {
+            int temp=heights[i];
+            heights[i]=heights[n-i-1];
+            heights[n-i-1]=temp;
+        }
+        for(int i=0;i<n;i++)
+        {
+            names[i]=map.get(heights[i]);
+        }
+        return names;
+    }
+}
